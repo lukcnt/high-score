e8b86551affa13cc1389b4df3ead6bd348e3e67c
Lucas Cantoni <lukcnt@outlook.com>
2022-08-11 01:39:03 -0300

    Added the for loop that finds the highest score.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 student_scores = input("Input a list of student scores with a space between each one : ").split()
+
+# Didn't use max function purposely to get a better grasp of for loops.
+highest_score = 0
+
+for score in student_scores:
+    if int(score) > highest_score:
+        highest_score = int(score)
